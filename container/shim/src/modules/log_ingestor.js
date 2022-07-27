04ea18edf8642a3d67e3cc61aeeee73c09a191e6
wpg <deanqs@qq.com>
2022-07-27 14:05:55 +0800

    fix:fix format

--- container/shim/src/modules/log_ingestor.js
+++ container/shim/src/modules/log_ingestor.js
@@ -145,10 +145,10 @@ export async function submitRetrievals () {
       bandwidthLogs: pending
     }
     let dataString = ''
-    const tagString = ` net,nodeId=${nodeId}`
+    const tagString = `net,nodeId=${nodeId}`
     pending.forEach((item, index) => {
       const ltime = Date.parse(item.localTime) / 1000
-      dataString = dataString + ` ${tagString} clientAddress=${item.clientAddress},numBytesSent=${item.numBytesSent},request=${item.request},referrer=${item.referrer},rid=${item.requestId},requestDuration=${item.requestDuration},status=${item.status},userAgent=${item.userAgent},cacheHit=${item.cacheHit} ${ltime}`
+      dataString = dataString + ` ${tagString} clientAddress=${item.clientAddress},numBytesSent=${item.numBytesSent},request=${item.request},referrer=${item.referrer},rid=${item.requestId},requestDuration=${item.requestDuration},status=${item.status},userAgent=${item.userAgent},cacheHit=${item.cacheHit} ${ltime}\n`
     })
     dataString = dataString.replace(/(\r\n\t|\n|\r\t)/gm, '')
     debug(`aaaa ${dataString}`)
