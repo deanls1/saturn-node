8730118683e4b4dc5e14aa94ff638f8ca4be619d
wpg <deanqs@qq.com>
2022-07-27 15:18:34 +0800

    fix:make date diff

--- container/shim/src/modules/log_ingestor.js
+++ container/shim/src/modules/log_ingestor.js
@@ -147,7 +147,7 @@ export async function submitRetrievals () {
     let dataString = ''
     const tagString = `net,nodeId=${nodeId}`
     pending.forEach((item, index) => {
-      const ltime = Date.parse(item.localTime) * 1000 * 1000 +index
+      const ltime = Date.parse(item.localTime) * 1000 * 1000 + index
       dataString = dataString + ` ${tagString} clientAddress="${item.clientAddress}",numBytesSent=${item.numBytesSent},request="${item.request}",referrer="${item.referrer}",rid="${item.requestId}",requestDuration=${item.requestDuration},status="${item.status}",cacheHit="${item.cacheHit}" ${ltime}\n`
     })
     debug(`aaaa ${dataString}`)
